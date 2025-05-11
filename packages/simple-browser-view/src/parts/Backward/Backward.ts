9dee8022176da1f9ca89bcf4f2dba4a17a0d4cf9
Le Vivilet <levivlet@pm.me>
2025-05-11 21:39:05 +0200

    feature: add backward command

---
+++ packages/simple-browser-view/src/parts/Backward/Backward.ts
@@ -0,0 +1,15 @@
+import type { SimpleBrowserState } from '../SimpleBrowserState/SimpleBrowserState.ts'
+import * as ElectronBrowserViewFunctions from '../ElectronBrowserViewFunctions/ElectronBrowserViewFunctions.js'
+
+export const backward = async (state: SimpleBrowserState): Promise<SimpleBrowserState> => {
+  const { browserViewId } = state
+  await ElectronBrowserViewFunctions.backward(browserViewId)
+  const { url, canGoBack, canGoForward } = await ElectronBrowserViewFunctions.getStats(browserViewId)
+  return {
+    ...state,
+    isLoading: false,
+    canGoBack,
+    canGoForward,
+    iframeSrc: url,
+  }
+}
