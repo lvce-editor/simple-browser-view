2406aaeb222643b0c261a0e91f47ffe49a1cd363
Le Vivilet <levivilet@pm.me>
2026-01-09 00:43:01 +0100

    feature: update preload path (#34)

--- packages/build/src/dev.js
+++ packages/build/src/dev.js
@@ -10,13 +10,14 @@ const main = async () => {
   execa(
     'npx',
 
-    ['electron', '.'],
+    ['electron', '.', '--wait'],
     {
       cwd: join(root, 'packages', 'server'),
       stdio: 'inherit',
       env: {
         ...process.env,
         LVCE_SHARED_PROCESS_PATH: join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'src', 'sharedProcessMain.js'),
+        LVCE_PRELOAD_URL: join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'preload', 'src', 'index.js'),
       },
     },
   )
