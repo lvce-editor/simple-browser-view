4999949e68296c2ba288bae3888d5db6630dbb65
Le Vivilet <levivilet@pm.me>
2026-09-02 15:03:38 +0000

    feature: enable new tab input e2e

--- packages/e2e/electron/src/simple-browser.new-tab-input.ts
+++ packages/e2e/electron/src/simple-browser.new-tab-input.ts
@@ -2,8 +2,6 @@ import type { ElectronTestContext } from './_responseTest.ts'
 import * as SimpleBrowser from './_simpleBrowser.ts'
 
 export const name = 'simple-browser.new-tab-input'
-// TODO enable when the published Electron editor includes the new tab input user-select style
-export const skip = 1
 
 export const test = async ({ expect, page }: ElectronTestContext): Promise<void> => {
   await SimpleBrowser.show(page)
