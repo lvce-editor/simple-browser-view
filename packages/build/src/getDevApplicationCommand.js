a7945c84449fd62c65df7f7b7b6f72da8a6a0ae9
Le Vivilet <levivilet@pm.me>
2026-09-02 10:29:55 +0000

    fix: isolate simple browser dev profile (#72)

--- packages/build/src/getDevApplicationCommand.js
+++ packages/build/src/getDevApplicationCommand.js
@@ -2,7 +2,7 @@ import { join } from 'node:path'
 
 export const getDevApplicationCommand = (root, executablePath = process.env.LVCE_EXECUTABLE_PATH || 'lvce') => {
   return {
-    args: ['--link', join(root, '.tmp', 'dist'), '--hot-reload', '--wait', root],
+    args: ['--link', join(root, '.tmp', 'dist'), '--hot-reload', `--user-data-dir=${join(root, '.tmp', 'user-data')}`, '--wait', root],
     command: executablePath,
     cwd: root,
   }
